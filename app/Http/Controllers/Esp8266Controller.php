5c4a6769b42e4821b423bf69dcb5e6de82ff7e72
juki3r <deepong25@gmail.com>
2025-09-30 10:19:46 +0800

    aa

--- app/Http/Controllers/Esp8266Controller.php
+++ app/Http/Controllers/Esp8266Controller.php
@@ -20,43 +20,28 @@ class Esp8266Controller extends Controller
             'status' => 'required|string',
         ]);
 
+        // // Find ESP device by API key
+        $esp = Esp8266::where('user_id', $request->user_id)->first();
+
+        // // Update device status (last_seen & status)
+        $esp->updateOrCreate([
+            'user_id' => $request->user_id,
+            'device_id' => $request->device_id,
+            'last_seen' => now(),
+            'device_status' => $request->status, // make sure you have a column for this
+        ]);
+
+
+        if (!$esp) {
+            return response()->json(['status' => 'error'], 401);
+        }
+
         return response()->json([
             'status' => 'success',
             'api_key' => $apiKey,
             'user' => $user,
             'user_id' => $request->user_id,
+            ''
         ]);
-
-        // $apiKey = $request->header('X-API-KEY');
-
-        // // Find user/device by API key
-        // $user = User::where('api_key', $apiKey)->first();
-
-        // if (!$user) {
-        //     return response()->json(['status' => 'error', 'message' => 'Unauthorized'], 401);
-        // }
-
-        // $request->validate([
-        //     'user_id' => 'required|string',
-        //     'device_id' => 'required|string',
-        //     'status' => 'required|string',
-        // ]);
-
-        // // Find ESP device by API key
-        // $esp = Esp8266::where('device_id', $request->device_id)->first();
-
-        // // Update device status (last_seen & status)
-        // $esp->updateOrCreate([
-        //     'user_id' => $request->user_id,
-        //     'device_id' => $request->device_id,
-        //     'last_seen' => now(),
-        //     'device_status' => $request->status, // make sure you have a column for this
-        // ]);
-
-        // return response()->json([
-        //     'status' => 'success',
-        //     'last_seen' => $user->last_seen,
-        //     'device_status' => $user->device_status
-        // ]);
     }
 }
