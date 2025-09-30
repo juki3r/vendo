068ca5a7dc6b7ece694312a019174126a3791c77
juki3r <deepong25@gmail.com>
2025-09-30 15:29:19 +0800

    aaa

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Esp8266Controller;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
