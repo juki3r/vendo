a4081c785664e57c89cf11ef4ddcb4d33ff0e023
juki3r <deepong25@gmail.com>
2025-09-30 08:16:17 +0800

    aaa

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PostController;
+
+
+Route::get('/ping', function () {
+    return response()->json(['message' => 'API is working!']);
+});
