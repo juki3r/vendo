5acc57eb3d0d6218fbdd4a273a28d2046e1103b2
juki3r <deepong25@gmail.com>
2025-10-01 23:15:41 +0800

    aa

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -4,14 +4,13 @@
             {{ __('Sales') }}
         </h2>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 overflow-x-auto">
-                    <div class="table-responsive">
+<div class="py-12">
+    <div class="container">
+        <div class="card shadow-sm">
+            <div class="card-body">
+                <div class="table-responsive">
                     <table class="table table-striped table-bordered table-hover table-sm">
-                        <thead class="">
+                        <thead>
                             <tr>
                                 <th>#</th>
                                 <th>Voucher</th>
@@ -22,26 +21,27 @@
                         <tbody>
                             @forelse($sales as $sale)
                                 <tr>
-                                    <td >{{ $loop->iteration + ($sales->currentPage() - 1) * $sales->perPage() }}</td>
+                                    <td>{{ $loop->iteration + ($sales->currentPage() - 1) * $sales->perPage() }}</td>
                                     <td>{{ $sale->voucher }}</td>
-                                    <td >{{ $sale->minutes }}</td>
+                                    <td>{{ $sale->minutes }}</td>
                                     <td>{{ $sale->created_at->format('Y-m-d H:i') }}</td>
                                 </tr>
                             @empty
                                 <tr>
-                                    <td colspan="4">No sales found.</td>
+                                    <td colspan="4" class="text-center">No sales found.</td>
                                 </tr>
                             @endforelse
                         </tbody>
                     </table>
-                    </div>
-                    <!-- Bootstrap Pagination -->
-                    <div class="mt-3">
-                        {{ $sales->links() }}
-                    </div>
+                </div>
 
+                <!-- Bootstrap Pagination -->
+                <div class="mt-3">
+                    {{ $sales->links() }}
                 </div>
             </div>
         </div>
     </div>
+</div>
+
 </x-app-layout>
