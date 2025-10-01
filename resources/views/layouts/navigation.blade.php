e446aea3122fbe14216613013e13cf26b4755750
juki3r <deepong25@gmail.com>
2025-10-01 14:27:18 +0800

    aaa

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -20,7 +20,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('esp8266s.index')" :active="request()->routeIs('esp8266s.index')">
-                        {{ __('Esp8266') }}
+                        {{ __("Vendo's") }}
                     </x-nav-link>
                 </div>
             </div>
@@ -76,6 +76,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+        </div>
+
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('esp8266s.index')" :active="request()->routeIs('esp8266s.index')">
+                {{ __("Vendo's") }}
             </x-responsive-nav-link>
         </div>
 
