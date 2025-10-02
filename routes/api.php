85e2b8171029d858dedaaacd3bea7de08e561a1c
juki3r <deepong25@gmail.com>
2025-10-02 09:01:36 +0800

    aa

--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,4 @@ Route::get('/ping', function () {
 
 Route::post('/onlineCheck', [Esp8266Controller::class, 'onlineCheck']);
 Route::post('/responseUpdate', [Esp8266Controller::class, 'responseUpdate']);
-Route::post('/sales', [Esp8266Controller::class, 'storeVendoSales']);
+Route::post('/salesHistory', [Esp8266Controller::class, 'storeVendoSales']);
